df70bba213d122c539af48a1d1201bd572771097
David Rasmussen <123663853+DJR-Harvard@users.noreply.github.com>
2024-07-04 10:37:11 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def upload_file():
         return jsonify({'probability': float(probability)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
